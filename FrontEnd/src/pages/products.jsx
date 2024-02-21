ab7bc126d36b3bff929a27414701a114de4def7f
Paolo Damen <paolodamen@gmail.com>
2024-02-21 20:06:08 +0100

    add components

--- FrontEnd/src/pages/products.jsx
+++ FrontEnd/src/pages/products.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from 'react'
+import Product from "../components/ProductCard"
+import productService from '../services/productService'
 
+
+const Products = () => {
+
+    const [products, setProducts] = useState([]);
+
+const getProducts = async() => {
+              const products = await productService.getProducts()
+              setProducts(products)
+         };
+
+useEffect(() => {
+    getProducts()
+},[]);
+
+    return (
+       <>
+        
+            <ul>
+                {projects.map((project) => (
+                <Product product={product} key={product.id}/>
+                ))}
+            </ul>
+         
+        </>
+    )
+    
+}
+
+export default Products
 
 
