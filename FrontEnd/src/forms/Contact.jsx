4288843fec7aea70f17c949cc029703c4600a8f1
RaquelLores <128092239+RaquelLores@users.noreply.github.com>
2024-02-22 13:29:51 +0100

    branchupdate

---
+++ FrontEnd/src/forms/Contact.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+
+
+function Contact() {
+    const { register, handleSubmit } = useForm();
+    const [username, setUsername] = useState('');
+    const navigate = useNavigate();
+
+    const onSubmit = (data) => {
+        setUsername(data.username);
+        navigate('/home', { state: { username: data.username } });
+    };
+
+    return (
+        <>
+            <section className='logoandlogin'>
+                <div>
+                    <img className='logo' src={logo} alt="logo"></img>
+                </div>
+
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className='label'>
+                            <label htmlFor='username'>
+                                <input id='username' {...register('username')} type='text' placeholder='Tu nombre:' />
+                            </label>
+                        </div>
+                        <div className='label'>
+                            <label htmlFor='email'>
+                                <input id='email' {...register('email')} required type='email' placeholder='Email:' />
+                            </label>
+                        <div className='label'>
+                            <label htmlFor='message'>
+                                <input id='message' {...register('message')} required type='message' placeholder='Tu mensaje:' />
+                            </label>
+                        </div>    
+
+                    
+                        </div>
+                        <button className='access' type="submit">ACCEDER</button>
+                    </form>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Contact;
