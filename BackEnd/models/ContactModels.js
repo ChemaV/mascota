d43789100b60f75bde45f6bd98e6b5c89ba1587e
Desiree <desireesanchezdev@gmail.com>
2024-02-22 21:28:04 +0100

    Change functions for send form Contact

--- BackEnd/models/ContactModels.js
+++ BackEnd/models/ContactModels.js
@@ -1,11 +1,13 @@
-import db from "../db.js"
-import { DataTypes} from "sequelize"
+import db from "../db.js";
+import { DataTypes } from "sequelize";
 
-const ContactModels=db.define("contact",{
-    name:{type:DataTypes.STRING},
-    email:{type:DataTypes.STRING},
-    message:{type:DataTypes.STRING},
-    tlf:{type:DataTypes.STRING},
-})
+const ContactModels = db.define("contacts", {
+    name: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING },
+    message: { type: DataTypes.STRING },
+    tlf: { type: DataTypes.STRING },
+    createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    updatedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+});
+
 export default ContactModels;
-
